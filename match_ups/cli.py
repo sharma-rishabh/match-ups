31a85c0ab93227434b63a0392acb408b37ebd622
Rishabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2024-04-18 17:52:50 +0530

    Add validations for arguments and options and add help text

--- match_ups/cli.py
+++ match_ups/cli.py
@@ -20,14 +20,25 @@ from .create_match_ups import create_match_ups
     default=1,
 )
 def match_ups(participants: str, h2h_count: int, phases: int):
-    participants=participants.split(",")
+    """
+    Create matchups from a list of participants.
+
+    PARTICIPANTS - Comma separated list of participants. Ex: "Team1,Team2"
+    """
+
+    participants = participants.split(",")
+
+    if len(participants) < 2:
+        raise click.UsageError("Participant count can not be less than 2")
+
+    if phases == 0:
+        raise click.UsageError("Phases can not be less than 1")
+
+    if h2h_count == 0:
+        raise click.UsageError("h2h-count can not be less than 1")
+
     result = create_match_ups(participants, h2h_count, phases)
 
-    for phase,res in enumerate(result):
+    for phase, res in enumerate(result):
         click.secho(f"Phase {phase+1}", bold=True)
-        click.echo(
-            tabulate(
-                res,
-                tablefmt="grid"
-            )
-        )
+        click.echo(tabulate(res, tablefmt="grid"))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,15 +6,16 @@ from click.testing import CliRunner
 
 @patch("match_ups.cli.create_match_ups")
 def test_arguments_parsing(create_matchups):
-    create_matchups.return_value=[]
+    create_matchups.return_value = []
     runner = CliRunner()
     runner.invoke(match_ups, ["A,B,C", "--h2h-count", "3", "--phases", "4"])
     create_matchups.assert_called_once_with(["A", "B", "C"], 3, 4)
 
+
 def test_output():
     runner = CliRunner()
     result = runner.invoke(match_ups, ["A,B,C", "--h2h-count", "3", "--phases", "2"])
-    excepted = """Phase 1
+    expected = """Phase 1
 +---+---+
 | A | B |
 +---+---+
@@ -39,4 +40,28 @@ Phase 2
 """
     assert result.exit_code == 0
 
-    assert result.output == excepted
+    assert result.output == expected
+
+
+def test_single_participant_validation():
+    runner = CliRunner()
+    result = runner.invoke(match_ups, ["A"])
+    expected="Participant count can not be less than 2"
+    assert result.exit_code != 0
+    assert expected in result.output
+
+
+def test_phase_validation():
+    runner = CliRunner()
+    result = runner.invoke(match_ups, ["A,B", "--phases", "0"])
+    expected = "Phases can not be less than 1"
+    assert result.exit_code != 0
+    assert expected in result.output
+
+
+def test_h2h_count_validation():
+    runner = CliRunner()
+    result = runner.invoke(match_ups, ["A,B", "--h2h-count", "0"])
+    expected = "h2h-count can not be less than 1"
+    assert result.exit_code != 0
+    assert expected in result.output
